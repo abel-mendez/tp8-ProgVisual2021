dc1a4cc6f593fd42370457afb28e09e30c7391bc
LEANDRO VICTORINO CRUZ <83726994+Leo-apu@users.noreply.github.com>
2021-06-01 06:14:38 -0300

    Completando validaciones en los controller y en los template.

--- src/main/java/ar/edu/unju/fi/tp8/controller/ProductoController.java
+++ src/main/java/ar/edu/unju/fi/tp8/controller/ProductoController.java
@@ -2,12 +2,15 @@ package ar.edu.unju.fi.tp8.controller;
 
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -46,11 +49,18 @@ public class ProductoController {
 	}
 	
 	@PostMapping("/producto/guardar")
-	public String addProducto(@ModelAttribute("producto") Producto unProducto) {
+	public String addProducto(@Valid @ModelAttribute("producto") Producto unProducto, BindingResult result, Model model) {
 		LOGGER.info("CONTROLLER : ProductoController with /addProducto post method");
-		productoService.addProducto(unProducto);
-		LOGGER.info("RESULT : VISUALIZA LA PAGINA resultado.html ");
-		return "resultado";
+		
+		if (result.hasErrors()) {
+			model.addAttribute("producto",unProducto);
+			return "nuevo";
+		}else {
+			productoService.addProducto(unProducto);
+			LOGGER.info("RESULT : VISUALIZA LA PAGINA resultado.html ");
+			return "resultado";
+		}
+		
 	}
 	
 	
